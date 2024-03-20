683c78bface51938ec116e0dba493597a127bc6e
Mukesh Pathak <mukesh.pathak@gmail.com>
2024-03-20 16:33:54 +0530

    added dvc

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 import os
-from src.cnnClassifier.constants import *
-from src.cnnClassifier.utils.common import read_yaml, create_directories, save_json
-from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,
+from cnnClassifier.constants import *
+from cnnClassifier.utils.common import read_yaml, create_directories, save_json
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                     PrepareBaseModelConfig,
                                                     TrainingConfig,
                                                     EvaluationConfig)
